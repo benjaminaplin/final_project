eec41c7bf8dfa7900fba483093a2cdedcd5a8c36
Benjamin Aplin <benjaminaplin@gmail.com>
2015-09-17 22:24:33 -0400

    add d3 chart features

--- public/backbone/backbone_script.js
+++ public/backbone/backbone_script.js
@@ -1,124 +1,120 @@
-console.log("backbone linked");
-var candidatesArray = []
-var CandidateApp = {
-  Models: {},
-  Collections: {},
-  Views: {},
-  Data: {
-    candidatesArray:[]
-  }
-  //decide what i want to store: global variables, models, views, collecitons
-};
-//------------CANDIDATE-COLLECTION-VIEW------------------------------
+// console.log("backbone linked");
+// var candidatesArray = []
+// var CandidateApp = {
+//   Models: {},
+//   Collections: {},
+//   Views: {},
+//   Data: {
+//     candidatesArray:[]
+//   }
+//   //decide what i want to store: global variables, models, views, collecitons
+// };
+// //------------CANDIDATE-COLLECTION-VIEW------------------------------
 
-CandidateApp.Models.Candidate = Backbone.Model.extend({
-  url: '/candidates',
-})
+// CandidateApp.Models.Candidate = Backbone.Model.extend({
+//   url: '/candidates',
+// })
 
-//------------CANDIDATE-COLLECTION------------------------------
+// //------------CANDIDATE-COLLECTION------------------------------
 
-CandidateApp.Collections.CandidateCollection = Backbone.Collection.extend({
-  model: CandidateApp.Models.Candidate,
-  url: '/candidates',
-  apiCall: function(){
-    var that = this;
-    var candidates = [
-      // 'chafee': {},
-      {'name':'Clinton', 'candId': 'P00003392', 'commId': 'C00575795'},
-      {'name':'O\'Malley', 'candId': 'P60007671', 'commId': 'C00578658'},
-      {'name':'Sanders', 'candId': 'P60007168', 'commId': 'C00577130'},
-      // {'name':'webb', 'candId': 'P60008885', 'commId': 'C00581215'},
-      {'name':'Bush', 'candId': 'P60008059', 'commId': 'C00579458'},
+// CandidateApp.Collections.CandidateCollection = Backbone.Collection.extend({
+//   model: CandidateApp.Models.Candidate,
+//   url: '/candidates',
+//   apiCall: function(){
+//     var that = this;
+//     var candidates = [
+//       // 'chafee': {},
+//       {'name':'Clinton', 'candId': 'P00003392', 'commId': 'C00575795'},
+//       {'name':'O\'Malley', 'candId': 'P60007671', 'commId': 'C00578658'},
+//       {'name':'Sanders', 'candId': 'P60007168', 'commId': 'C00577130'},
+//       // {'name':'webb', 'candId': 'P60008885', 'commId': 'C00581215'},
+//       {'name':'Bush', 'candId': 'P60008059', 'commId': 'C00579458'},
 
 
-      {'name':'Carson','candId': 'P60005915', 'commId': 'C00573519'},
-      // // {'name':'christie','candId': 'P60008521', 'commId': 'C00580399'},
-      {'name':'Cruz','candId': 'P60006111', 'commId': 'C00574624'},
-      {'name': 'Fiorina','candId': 'P60007242', 'commId': 'C00577312'},
-      // // {'name':'gilmore': {},
-      {'name':'Graham','candId': 'P60007697', 'commId':'C00578757'},
-      {'name':'Huckabee','candId': 'P80003478', 'commId': 'C00577981'},
-      {'name':'Jindal','candId': 'P60008398', 'commId': 'C00580159'},
-      // // {'name':'kasich','candId': 'P60003670', 'commId': 'C00581876'},
-      // // '{'name':'pataki': {},
-      {'name':'Paul','candId': 'P40003576', 'commId': 'C00575449'},
-      {'name':'Rubio','candId': 'P60006723', 'commId': 'C00458844'},
-      {'name':'Santorum','candId': 'P20002721', 'commId': 'C00578492'},
-      {'name':'Trump','candId': 'P80001571', 'commId': 'C00580100' },
-      // {'name':'walker','candId': 'P60006046', 'commId': 'C00580480'}
-  ]
-    candidates.forEach(function(e){
-      var commId = e.commId;
-      $.ajax({
-        url: 'https://api.open.fec.gov/v1/committee/' + commId + '/totals/?api_key=vEAnpDMm3fLd0ZT4eVb2ULbrVlOJ4XvQA50txDkG&page=1&per_page=20&sort_nulls_large=true&sort=-cycle',
-        method: 'GET',
-        dataType: 'json',
-        success: function(data) {
-          //set each attribute that we need 
-          // console.log(data)
-          //create instances of each candidate model and view, render the view
-          var newCandidate = new CandidateApp.Models.Candidate({
-            receipts: data.results[0].receipts,
-            commId: data.results[0].committee_id,
-            candContr: data.results[0].candidate_contribution,
-            candSpent: data.results[0].disbursements
-          });
+//       {'name':'Carson','candId': 'P60005915', 'commId': 'C00573519'},
+//       // // {'name':'christie','candId': 'P60008521', 'commId': 'C00580399'},
+//       {'name':'Cruz','candId': 'P60006111', 'commId': 'C00574624'},
+//       {'name': 'Fiorina','candId': 'P60007242', 'commId': 'C00577312'},
+//       // // {'name':'gilmore': {},
+//       {'name':'Graham','candId': 'P60007697', 'commId':'C00578757'},
+//       {'name':'Huckabee','candId': 'P80003478', 'commId': 'C00577981'},
+//       {'name':'Jindal','candId': 'P60008398', 'commId': 'C00580159'},
+//       // // {'name':'kasich','candId': 'P60003670', 'commId': 'C00581876'},
+//       // // '{'name':'pataki': {},
+//       {'name':'Paul','candId': 'P40003576', 'commId': 'C00575449'},
+//       {'name':'Rubio','candId': 'P60006723', 'commId': 'C00458844'},
+//       {'name':'Santorum','candId': 'P20002721', 'commId': 'C00578492'},
+//       {'name':'Trump','candId': 'P80001571', 'commId': 'C00580100' },
+//       // {'name':'walker','candId': 'P60006046', 'commId': 'C00580480'}
+//   ]
+//     candidates.forEach(function(e){
+//       var commId = e.commId;
+//       $.ajax({
+//         url: 'https://api.open.fec.gov/v1/committee/' + commId + '/totals/?api_key=vEAnpDMm3fLd0ZT4eVb2ULbrVlOJ4XvQA50txDkG&page=1&per_page=20&sort_nulls_large=true&sort=-cycle',
+//         method: 'GET',
+//         dataType: 'json',
+//         success: function(data) {
+//           //set each attribute that we need 
+//           // console.log(data)
+//           //create instances of each candidate model and view, render the view
+//           var newCandidate = new CandidateApp.Models.Candidate({
+//             receipts: data.results[0].receipts,
+//             commId: data.results[0].committee_id,
+//             candContr: data.results[0].candidate_contribution,
+//             candSpent: data.results[0].disbursements
+//           });
             
-          var newCandidateView = new CandidateApp.Views.CandidateView({model: newCandidate});
+//           var newCandidateView = new CandidateApp.Views.CandidateView({model: newCandidate});
 
-          that.add(newCandidate);
-        },
-        error: function(error) {
-          console.log(error);
-          console.log('err')
-        }
-       })//$ajax
-    })//end loop
-  }
-});
+//           that.add(newCandidate);
+//         },
+//         error: function(error) {
+//           console.log(error);
+//           console.log('err')
+//         }
+//        })//$ajax
+//     })//end loop
+//   }
+// });
 
-//------------CANDIDATE-VIEW------------------------------------
+// //------------CANDIDATE-VIEW------------------------------------
  
-CandidateApp.Views.CandidateView = Backbone.View.extend({
-  initialize: function(){
-    this.listenTo(this.model, "change", this.render);
-  },
-  tagName: 'li', 
-  template: _.template($("#main-template").text()),
+// CandidateApp.Views.CandidateView = Backbone.View.extend({
+//   initialize: function(){
+//     this.listenTo(this.model, "change", this.render);
+//   },
+//   tagName: 'li', 
+//   template: _.template($("#main-template").text()),
 
-  render: function(){
+//   render: function(){
 
-    var renderedHTML = this.$el.html(this.template(this.model.attributes));
-    this.$el.html(renderedHTML);
+//     var renderedHTML = this.$el.html(this.template(this.model.attributes));
+//     this.$el.html(renderedHTML);
 
-    $("#candidates").append(this.$el)
-  }
-});
+//     $("#candidates").append(this.$el)
+//   }
+// });
 
-//------------CANDIDATE-COLLECTION-VIEW------------------------------------
+// //------------CANDIDATE-COLLECTION-VIEW------------------------------------
 
-CandidateApp.Views.CandidateCollectionView = Backbone.View.extend({
-  //render candidate view and append to .this (self) here and append 
-  el: $('#chart'),
-  initialize: function(){
-    this.listenTo(this.collection, 'add', this.populateArray)
-    // this.populateArray()
-  },
-  populateArray: function(model){
-    // debugger
-    candidatesArray.push(model.attributes)
-    // console.log(candidatesArray)
-    console.log("hello")
-  }
-});
+// CandidateApp.Views.CandidateCollectionView = Backbone.View.extend({
+//   //render candidate view and append to .this (self) here and append 
+//   el: $('#chart'),
+//   initialize: function(){
+//     this.listenTo(this.collection, 'add', this.populateArray)
+//     // this.populateArray()
+//   },
+//   populateArray: function(model){
+//     // debugger
+//     candidatesArray.push(model.attributes)
+//     // console.log(candidatesArray)
+//     console.log("hello")
+//   }
+// });
 
-var candidates = new CandidateApp.Collections.CandidateCollection({});
-var candidateCollectionView = new CandidateApp.Views.CandidateCollectionView({collection: candidates})
-candidates.apiCall();
+// var candidates = new CandidateApp.Collections.CandidateCollection({});
+// var candidateCollectionView = new CandidateApp.Views.CandidateCollectionView({collection: candidates})
+// candidates.apiCall();
 
- // console.log('need it here', candidatesArray)
-
-
-
-var ben = "hello"
+// var ben = "hello"
 
--- public/d3/d3_script.js
+++ public/d3/d3_script.js
@@ -1,52 +1,79 @@
-console.log("dee three linked");
-// console.log("ben says ", ben)
-window.setTimeout(function(){
+// console.log("dee three linked");
+// // console.log("ben says ", ben)
+// window.setTimeout(function(){
 
 
+// candidatesArray.forEach(function(e){
+//  if(e.commId === 'C00575795'|| e.commId === 'C00578658' || e.commId === 'C00577130'){
+//    e.color = 'blue'
+//   } else {
+//    e.color = 'red'
+//   }
+// })
+// // use switch statement here instead
+// candidatesArray.forEach(function(e){
+//   if(e.commId === 'C00575795'){
+//     e.name = 'Hillary Clinton';
+//   } else if (e.commId === 'C00578658') {
+//     e.name = 'O\'Malley';
+//   } else if (e.commId === 'C00577130') {
+//     e.name = 'Bernie Sanders';
+//   } else if (e.commId === 'C00579458') {
+//     e.name = 'Jeb Bush';
+//   } else if (e.commId === 'C00573519') {
+//     e.name = 'Ben Carson';
+//   } else if (e.commId === 'C00574624') {
+//     e.name = 'Ted Cruz';
+//   } else if (e.commId === 'C00577312') {
+//     e.name = 'Carly Fiorina';
+//   } else if (e.commId === 'C00578757') {
+//     e.name = 'Linsey Graham';
+//   } else if (e.commId === 'C00577981') {
+//     e.name = 'Huckabee';
+//   } else if (e.commId === 'C00580159') {
+//     e.name = 'Bobby Jindal';
+//   } else if (e.commId === 'C00575449') {
+//     e.name = 'Rand Paul';
+//   } else if (e.commId === 'C00458844') {
+//     e.name = 'Marco Rubio';
+//   } else if (e.commId === 'C00578492') {
+//     e.name = 'Rick Santorum'
+//   } else if (e.commId === 'C00580100'){
+//     e.name = 'The Donald'
+//   }
+// })
+
+var candidatesArray = [
+  {'candContr': 0, 'candSpent': 3085615, 'color': 'blue', 'commId': 'C00577130', 'name': "Bernie Sanders", 'receipts': 15247353},
+  {'candContr': 4049, 'candSpent': 1414674, 'color': 'red', 'commId': 'C00580100', 'name': "The Donald", 'receipts': 1902410},
+  {'candContr': 388720, 'candSpent': 3078087, 'color': 'red', 'commId': 'C00579458', 'name': "Jeb Bush", 'receipts': 11429897}, 
+  {'candContr': 0, 'candSpent': 5896929, 'color': 'red', 'commId': 'C00573519', 'name': "Ben Carson", 'receipts': 10642242},
+  {'candContr': 0, 'candSpent': 692768 ,'color': 'blue', 'commId': 'C00578658', 'name': "O'Malley", 'receipts': 2006904},
+  {'candContr': 0, 'candSpent': 2771264, 'color': 'red', 'commId': 'C00575449', 'name': "Rand Paul", 'receipts': 6932779},
+  {'candContr': 0, 'candSpent': 714044 ,'color': 'red', 'commId': 'C00577312', 'name': "Carly Fiorina", 'receipts': 1704703},
+  {'candContr': 278821, 'candSpent': 18699814, 'color': 'blue', 'commId': 'C00575795', 'name': "Hillary Clinton", 'receipts': 47549949},
+  {'candContr': 0, 'candSpent': 5821564, 'color': 'red', 'commId': 'C00574624', 'name': "Ted Cruz", 'receipts': 14349160},
+  {'candContr': 0, 'candSpent': 375598,'color': 'red', 'commId': 'C00578492', 'name': "Rick Santorum", 'receipts': 607617},
+  {'candContr': 0, 'candSpent': 1118991, 'color': 'red', 'commId': 'C00577981', 'name': "Huckabee", 'receipts': 2004462},
+  {'candContr': 0, 'candSpent': 65043, 'color': 'red', 'commId': 'C00580159', 'name': "Bobby Jindal", 'receipts': 578758},
+  {'candContr': 0, 'candSpent': 190536 ,'color': 'red', 'commId': 'C00458844', 'name': "Marco Rubio", 'receipts': 917946},
+  {'candContr': 0, 'candSpent': 1126733, 'color': 'red', 'commId': 'C00578757', 'name': "Linsey Graham", 'receipts': 3709552}
+]
+
+receiptsArray = [];
 candidatesArray.forEach(function(e){
- if(e.commId === 'C00575795'|| e.commId === 'C00578658' || e.commId === 'C00577130'){
-   e.color = 'blue'
-  } else {
-   e.color = 'red'
-  }
-})
-// use switch statement here instead
-candidatesArray.forEach(function(e){
-  if(e.commId === 'C00575795'){
-    e.name = 'Hillary Clinton';
-  } else if (e.commId === 'C00578658') {
-    e.name = 'O\'Malley';
-  } else if (e.commId === 'C00577130') {
-    e.name = 'Bernie Sanders';
-  } else if (e.commId === 'C00579458') {
-    e.name = 'Jeb Bush';
-  } else if (e.commId === 'C00573519') {
-    e.name = 'Ben Carson';
-  } else if (e.commId === 'C00574624') {
-    e.name = 'Ted Cruz';
-  } else if (e.commId === 'C00577312') {
-    e.name = 'Carly Fiorina';
-  } else if (e.commId === 'C00578757') {
-    e.name = 'Linsey Graham';
-  } else if (e.commId === 'C00577981') {
-    e.name = 'Huckabee';
-  } else if (e.commId === 'C00580159') {
-    e.name = 'Bobby Jindal';
-  } else if (e.commId === 'C00575449') {
-    e.name = 'Rand Paul';
-  } else if (e.commId === 'C00458844') {
-    e.name = 'Marco Rubio';
-  } else if (e.commId === 'C00578492') {
-    e.name = 'Rick Santorum'
-  } else if (e.commId === 'C00580100'){
-    e.name = 'The Donald'
-  }
+  receiptsArray.push(e.receipts);
 })
 
-console.log(candidatesArray)
+receiptsArray.sort(function compaireNumbers(a,b){
+  return a -b;
+});
 
-var height = 500, 
-    width = 600, 
+var margin = { top: 40, right: 10, bottom:40, left:115}
+
+//have to remove the above margins from the size below
+var height = 550 - margin.top - margin.bottom, 
+    width = 600 - margin.left - margin.right, 
     barWidth = 50, 
     barOffset = 5;
 
@@ -54,22 +81,26 @@ var tempColor;
 //for mouseover    
 
 var yScale = d3.scale.linear()
-  .domain([0, d3.max(candidatesArray)])
+  .domain([0, d3.max(receiptsArray)])
   .range([0, height]) 
 
 var xScale = d3.scale.ordinal()
-  .domain(d3.range(0, candidatesArray.length))
-  .rangeBands([0, width]) 
+  .domain(d3.range(0, receiptsArray.length))
+  //second integer argument is space btw bars
+  .rangeBands([0, width], 0.15) 
 
 var tooltip = d3.select('body').append('div')
   .style('position', 'absolute')
   .style('padding', ' 0 10px')
   .style('opacity', 0)      
 
+//creat chart here
 var myChart = d3.select('#chart').append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .style('background', '#C9D7D6')
+  .style('background', '#E7E0CB')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', 'translate('+ margin.left + ','+ margin.top + ')')
   .selectAll('rect').data(candidatesArray)
   .enter().append('rect')
     .attr('width', xScale.rangeBand())
@@ -90,9 +121,9 @@ var myChart = d3.select('#chart').append('svg')
     tooltip.transition()
       .style('opacity', .9)
 
-    tooltip.html(d.receipts) 
+    tooltip.html(d.receipts, d.name) 
       .style('left', (d3.event.pageX) + 'px')
-      .style('top', (d3.event.pageY) + 'px') 
+      .style('top', (d3.event.pageY) + 'px')
 
     tempColor = this.style.fill;
     d3.select(this)
@@ -105,6 +136,7 @@ var myChart = d3.select('#chart').append('svg')
       .style('fill', tempColor)
   })   
 //
+
 myChart.transition()
     .attr('height', function(d){
           return d.receipts/100000
@@ -120,34 +152,42 @@ myChart.transition()
     .duration(1000)
     .ease('elastic')
 
+var vGuideScale = d3.scale.linear()
+  .domain([0, d3.max(receiptsArray)])
+  .range([height,0])
 
-}, 1500)
+var vAxis = d3.svg.axis()
+    .scale(vGuideScale)
+    .orient('left')
+    .ticks(10)
 
+var vGuide = d3.select('svg').append('g')
+    vAxis(vGuide)
+    //translate moves axis 
+    //changing the translate command to move with the margins
+    vGuide.attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')')
+    vGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    vGuide.selectAll('line')
+        .style({ stroke: "#000"})
 
+var hAxis = d3.svg.axis()
+    //scale created on line 78
+    .scale(xScale)
+    .orient('bottom')
+    .tickValues(xScale.domain().filter(function(d, i) {
+        return receiptsArray.length;
+        // below would place tick every 5 bars
+        // return !(i % (candidatesArray.length/5));
+    }))
 
+var hGuide = d3.select('svg').append('g')
+    hAxis(hGuide)
+    hGuide.attr('transform', 'translate(' + margin.left + ', ' + (height + margin.top) + ')')
+    hGuide.selectAll('path')
+        .style({ fill: 'none', stroke: "#000"})
+    hGuide.selectAll('line')
+        .style({ stroke: "#000"})
 
+// }, 1500)
 
-
-
-// var candidatesArray = [  
-//       {'name':'Clinton', 'color': 'blue', 'candId': 'P00003392', 'commId': 'C00575795','receipts': 47549949, 'candContr': 278821, 'candSpent': 18699814},
-//       {'name':'O\'Malley', 'color': 'blue','candId': 'P60007671', 'commId': 'C00578658', 'receipts': 2006904,  'candContr': 0, 'candSpent': 692768},
-//       {'name':'Sanders','color': 'blue', 'candId': 'P60007168', 'commId': 'C00577130', 'receipts': 15247353, 'candContr': 0, 'candSpent': 3085615},
-//       // {'name':'webb', 'candId': 'P60008885', 'commId': 'C00581215'},
-//       {'name':'Bush', 'color': 'red','candId': 'P60008059', 'commId': 'C00579458', 'receipts': 11429897, 'candContr': 388720, 'candSpent': 3078087},
-//       {'name':'Carson','color': 'red','candId': 'P60005915', 'commId': 'C00573519','receipts': 10642242, 'candContr': 0, 'candSpent': 5896929},
-//       // // {'name':'christie','candId': 'P60008521', 'commId': 'C00580399'},
-//       {'name':'Cruz','color': 'red','candId': 'P60006111', 'commId': 'C00574624', 'receipts': 14349160, 'candContr': 0, 'candSpent': 5821564},
-//       {'name': 'Fiorina','color': 'red','candId': 'P60007242', 'commId': 'C00577312',  'receipts': 1704703, 'candContr': 0, 'candSpent': 714044},
-//       // // {'name':'gilmore': {},
-//       {'name':'Graham','color': 'red','candId': 'P60007697', 'commId':'C00578757',  'receipts': 3709552, 'candContr': 0, 'candSpent': 1126733 },
-//       {'name':'Huckabee','color': 'red','candId': 'P80003478', 'commId': 'C00577981', 'receipts': 2004462, 'candContr': 0, 'candSpent': 1118991},
-//       {'name':'Jindal','color': 'red','candId': 'P60008398', 'commId': 'C00580159', 'receipts': 578758, 'candContr': 0, 'candSpent': 65043},
-//       // // {'name':'kasich','candId': 'P60003670', 'commId': 'C00581876'},
-//       // // '{'name':'pataki': {},
-//       {'name':'Paul','color': 'red','candId': 'P40003576', 'commId': 'C00575449', 'receipts': 6932779, 'candContr': 0, 'candSpent': 2771264},
-//       {'name':'Rubio','color': 'red','candId': 'P60006723', 'commId': 'C00458844', 'receipts': 917946, 'candContr': 0, 'candSpent': 190536},
-//       {'name':'Santorum','color': 'red','candId': 'P20002721', 'commId': 'C00578492', 'receipts': 607617, 'candContr': 0, 'candSpent': 375598},
-//       {'name':'Trump', 'color': 'red', 'candId': 'P80001571', 'commId': 'C00580100', 'receipts': 1902410, 'candContr': 4049, 'candSpent': 1414674},
-//       // {'name':'walker','candId': 'P60006046', 'commId': 'C00580480'}
-//       ];
